7e00e3f288878d3b152dca544590d42691e5bac8
Сергей Василевский <sarge14@mail.ru>
2019-09-03 02:50:28 +0300

    0.0.2

--- Crypto Portfolio/MainPage.xaml.cs
+++ Crypto Portfolio/MainPage.xaml.cs
@@ -33,13 +33,31 @@ namespace Crypto_Portfolio
         public MainPage()
         {
             this.InitializeComponent();
-            Coins = new List<Coin>();
-        }
+            if (Coins != null)
+            {
+                Coins = new List<Coin>();
+            }
+            var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var folderPath = localFolder.Path;
+            var filePath = Path.Combine(folderPath, this.DBName);
+            using (var db = new LiteDatabase(filePath))
+            {
+                var col = db.GetCollection<Coin>("coins");
+                Coins = new List<Coin>();
+                for (int i = 1; i <= 3; i++)
+                {
+                    var results = col.FindById(i);
+                    Coins.Add(results);
+                }
+            }
+            dataGrid.ItemsSource = null;
+            dataGrid.ItemsSource = Coins;
 
+        }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Coins = new List<Coin>
-               {
+            {
                        new Coin
                        {
                            Name = "XRP", Count = 2000, Price = "0,00002606", Amount = "1"
@@ -52,68 +70,22 @@ namespace Crypto_Portfolio
                        {
                            Name = "PLBT", Count = 8000, Price = "0,00015606", Amount = "1"
                        }
-               };
+            };
             var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
             var folderPath = localFolder.Path;
             var filePath = Path.Combine(folderPath, this.DBName);
             using (var db = new LiteDatabase(filePath))
             {
-                // Get a collection (or create, if doesn't exist)
                 var col = db.GetCollection<Coin>("coins");
-
-                //   var coins = new List<Coin>();
                 var coin = Coins;
-                /*   var coin = new Coin
-                   {
-                       Rank = 1,
-                       Name = "XRP",
-                       Count = 2000,
-                       Price = "0,00002606",
-                       Amount = "1"
-                   };*/
-
-
-                // col.Delete(coin);
-                /*    for (int i = 1; i < 3; i++)
-                    {
-                        col.Delete(i);
-                    }*/
                 col.Insert(coin);
-
-                // Index document using document Name property
-                //   col.EnsureIndex(x => x.Name);
-
-                // Use LINQ to query documents
-
-                // var results = col.Find(x => x.Name.StartsWith("EMC"));
                 var results = col.FindById(2);
                 if (results != null)
                 {
                     text.Text = results.Amount;
                 }
-
             }
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
-            var folderPath = localFolder.Path;
-            var filePath = Path.Combine(folderPath, this.DBName);
-            using (var db = new LiteDatabase(filePath))
-            {
-                // Get a collection (or create, if doesn't exist)
-                var col = db.GetCollection<Coin>("coins");
-                // var coin = Coins;
-                Coins = new List<Coin>();
-                for (int i = 1; i <= 3; i++)
-                {
-                    var results = col.FindById(i);
-                    Coins.Add(results);
-                }
-            }
-        }
-
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             double result;
@@ -121,8 +93,6 @@ namespace Crypto_Portfolio
             {
                 result = Coins[i].Count * double.Parse(Coins[i].Price);
                 Coins[i].Amount = result.ToString("F8");
-                //  Coins[i].Amount
-                //  text.Text += Coins[i].Amount;
             }
             dataGrid.ItemsSource = null;
             dataGrid.ItemsSource = Coins;
@@ -131,16 +101,14 @@ namespace Crypto_Portfolio
             var filePath = Path.Combine(folderPath, this.DBName);
             using (var db = new LiteDatabase(filePath))
             {
-                // Get a collection (or create, if doesn't exist)
                 var col = db.GetCollection<Coin>("coins");
                 var coin = Coins;
-
                 col.Update(coin);
-                var results = col.FindById(2);
+              /*  var results = col.FindById(2);
                 if (results != null)
                 {
                     text.Text = results.Amount;
-                }
+                }*/
             }
         }
     }
